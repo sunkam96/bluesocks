07ed18a29858bbdc6ef3d707eb6b2df34ee54ba1
sunkam96 <sunkam.sai@gmail.com>
2025-01-25 14:50:08 -0500

    allow all hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      "bluesocks.onrender.com",
-      "bluesocks.live",
-    ],
+    allowedHosts: true
   },
 })
