a01c501ff8a31da5709b90a50afb6043d6bae864
sunkam96 <sunkam.sai@gmail.com>
2025-02-08 19:55:11 -0500

    partial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,48 @@ function App() {
   return (
     <>
       <div className="container">
-        <h1>Bluesocks</h1>
-        <p>Welcome to bluesocks</p>
+        <BackgroundImageContainer/>
+        <ContentContainer/>
       </div>
     </>
+  )
+}
+
+function BackgroundImageContainer() {
+  return (
+    <div className="backgroundImageContainer">
+    </div>
+  )
+}
+
+
+function ContentContainer() {
+  return (
+    <div className="contentContainer">
+      <div className="labelContainer">
+        <p className="labelText">Book Now</p>
+      </div>
+      <div className="timeContainer">
+        <div className="dateContainer">
+          <p className="dateText">Thursday Feb 7 2025</p>
+        </div>
+        <div className="slotContainer">
+          <p className="availableSlotsText">Available Slots</p>
+          <div className="slot">
+            <p className="slotText">10:00 am - 11:00 am</p>
+          </div>
+          <div className="slot">
+            <p className="slotText">11:00 am - 12:00 am</p>
+          </div>
+          <div className="slot">
+            <p className="slotText">1:00 am - 2:00 am</p>
+          </div>
+          <div className="slot">
+            <p className="slotText">2:00 am - 3:00 am</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
